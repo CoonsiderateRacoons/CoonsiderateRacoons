fabd2fd37ed268f6218d9c2543d1f12c9d4e25c5
savybrandt <savybrandt@gmail.com>
2017-01-25 20:09:44 -0800

    wrote out App.js

--- public/components/App.js
+++ public/components/App.js
@@ -1,4 +1,30 @@
+class App extends React.component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			recipe: null
+		}
+	}
 
+	renderRecipe() {
+		this.props.searchRecipies()
+		.then(function(recipe) {
+			this.setState ({
+				recipe: recipe
+			})
+		})
+	}
+
+	render() {
+		return (
+		<div>
+			<button onClick={this.renderRecipe}>What's For Dinner?</button> <br/>
+			<RecipeView recipe={this.state.recipe} /> <br/>
+			<button>Let's Try it!</button><button onClick={this.renderRecipe}>Maybe Not...</button>
+		</div>
+		)
+	}
+}
 
 
 window.App = App;

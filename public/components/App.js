0553474c8bdc2bd2b1c72d5273f1088450e9916c
JARED SPENCER <jareddespencer@gmail.com>
2017-01-25 20:09:59 -0800

    Built out App.js

--- public/components/App.js
+++ public/components/App.js
@@ -1,4 +1,33 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      recipe: null
+    }
+  }
+
+  renderRecipe() {
+    this.props.searchRecipes()
+    .then((recipe) => {
+      this.setState({
+        recipe: recipe
+      });
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.renderRecipe}>What's For Dinner?</button> <br/>
+        <RecipeView recipe={this.state.recipe}/> <br/>
+        <button>Let's Try It!</button>
+        <button onClick={this.renderRecipe}>Maybe not...</button>
+      </div>
+    )
+  }
+
+};
 
 
 window.App = App;

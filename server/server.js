a8a783b5706c1e033f21815766227aa9f7e7d731
Smriti Goel <smritigoel@Smritis-MacBook-Pro.local>
2017-02-06 11:03:22 -0800

    code cleanup

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var app = express();
 app.use(parser.json());
 app.use(express.static('./../public'));
 
+
 app.get('/random', function(req, res) {
   request('http://food2fork.com/api/search?key=cbac066753a4efb0561542a3a5c1a93b', function (err, response, body) { //find a random recipe
     var parsedBody = JSON.parse(response.body);
@@ -16,11 +17,6 @@ app.get('/random', function(req, res) {
   })
 });
 app.get('/specific', function(req, res) {
-  // console.log('req.body', req.body);
-  // console.log('params', params);
-  // console.log('url', req.url);
-  // var url = req.url.split('=')[1];
-  // console.log('url', url);
   var rId = req.url.split('=')[1] || 35382;
   console.log('rId', rId);
   request('http://food2fork.com/api/get?key=cbac066753a4efb0561542a3a5c1a93b&rId=' + rId, function (err, response, body) { //find a specific recipe
